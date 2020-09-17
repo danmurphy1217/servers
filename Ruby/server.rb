409ffb2f90aefd732d75fbba3de476cf8208a3a3
Dan Murphy <danielmurph8@gmail.com>
2020-09-17 19:08:08 -0400

    add handleRequest method

--- Ruby/server.rb
+++ Ruby/server.rb
@@ -22,13 +22,30 @@ method (GET, POST, ETC...), path (file path
 to search for), version (HTTP version, 1.1 or 2)
 =end
     method, path, version = request.lines[0].split
-    # print components to console
-    puts method, path, version
+    
+    def handleRequest( path, method )
+      if method.downcase == "get"
+        fname = SERVER_ROOT + "/#{ path }"
+        puts fname
+        if File.file?(fname) == true
+          f = File.open("#{ fname }")
+          puts f.read
+        else
+          puts "File does not exist."
+        end
+      else if method.downcase == "post" || method.downcase == "put" || method.downcase == "delete" 
+        puts "I don't support #{ method } yet."
+      end
+    end 
+  end
+
+
     # return components from function
     {
-        method: method,
-        path: path,
-        version: version
+        :method => method,
+        :path => path,
+        :version => version,
+        :contents => handleRequest( path, method )
     }
 end
 
@@ -59,13 +76,18 @@ So, host -> :host
     header.gsub(":", "").downcase.to_sym
   end
 
+
+# server accepting incoming connections
+client = server.accept
 # run code between {} until ctrl+c
 loop {
-    client = server.accept
     # read max of 2048 bytes from client
     request = client.readpartial(2048)
     # put response to parseRequest()
-    puts parseRequest( request )
+    data = parseRequest( request )
+    puts "METHOD: " + data.fetch( :method )
+    puts "PATH: " + data.fetch( :path )
+    puts "HTTP VERSION: " + data.fetch( :version )
     # put response to parseHeaders()
     puts parseHeaders( request )
 }
